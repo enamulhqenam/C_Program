2e860df98aa93d51e33f665fba948f9cef6a5d8a
Enamulhqenam <enamulhqenam50607210@gmail.com>
2023-04-08 13:24:36 +0600

    input tow nuber and Subtract this number .

---
+++ basics/Subtract.c
@@ -0,0 +1,21 @@
+//Subtract between tow number .
+
+
+#include<stdio.h>
+
+int main()
+{
+
+    float num1 , num2 , sub;
+
+    printf("Enter first number : ");
+    scanf("%f",&num1);
+
+    printf("Enter second number : ");
+    scanf("%f",&num2);
+
+    sub = num1 - num2;
+
+    printf("Subtract number is = %.2f",sub);
+
+}
