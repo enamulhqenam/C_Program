d8fc4283de7df0be69ce38bd699b5aa8c5bf8438
Enamulhqenam <enamulhqenam50607210@gmail.com>
2023-04-08 12:18:05 +0600

    solve addition proble . input 2 intiger number and sum this tow number .

---
+++ basics/addition.c
@@ -0,0 +1,16 @@
+// add 2 number .
+#include<stdio.h>
+
+int main()
+{
+
+    int num1  , num2  , sum ;
+
+    printf("Enter tow number : ");
+    scanf("%d %d", &num1,&num2);
+
+    sum = num1 + num2 ;
+    printf("Total Sum of 'num1 :%d' & 'num2 :%d'  =  %d",num1,num2,sum);
+
+    return 0;
+}
