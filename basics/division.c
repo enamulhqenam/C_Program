c344980ac7d9e5f25c98ff82556a2206c75ad3a2
Enamulhqenam <enamulhqenam50607210@gmail.com>
2023-04-09 13:13:38 +0600

    input tow number and solve multiplication and division

---
+++ basics/division.c
@@ -0,0 +1,20 @@
+//division
+
+#include<stdio.h>
+
+int main()
+{
+
+    float num1 , num2 , div ;
+
+    printf("Enter first number : ");
+    scanf("%f", &num1);
+
+    printf("Enter Second number : ");
+    scanf("%f", &num2);
+
+    div = num1/num2 ;
+
+    printf("Total division id %.2f",div);
+
+}
---
+++ basics/multiplication.c
@@ -0,0 +1,19 @@
+//multiplication
+
+#include<stdio.h>
+
+int main()
+{
+
+    int num1 , num2 , mul ;
+
+    printf("Enter the first number : ");
+    scanf("%d",&num1);
+
+    printf("Enter the second number : ");
+    scanf("%d",&num2);
+
+    mul = num1 * num2 ;
+
+    printf("Total multiplication is : %d",mul);
+}
